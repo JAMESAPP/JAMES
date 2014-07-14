3fef9788f37f26c4cf1b8d28b334e5728b76c041
Jefferson Campos <foguinho.peruca@gmail.com>
2014-07-13 22:16:11 -0300

    Backup
    Fixed loading

--- app/js/views/backup.js
+++ app/js/views/backup.js
@@ -19,8 +19,6 @@ define([
 		, template: Template
 
 		, initialize: function() {
-			var self = this;
-
 			this.expenses = [];
 			this.foods = [];
 			this.groceries = [];
@@ -29,14 +27,15 @@ define([
 			this.timesheets = [];
 			this.configurations = [];
 
-			this.transaction = App.indexedDB.db.transaction(['timesheets', 'expenses', 'foods', 'groceries', 'gyms', 'motorcycles', 'timesheets', 'configurations'], 'readonly');
-			var	expenseCursor = this.transaction.objectStore('expenses').openCursor(),
-				foodCursor = this.transaction.objectStore('foods').openCursor(),
-				groceryCursor = this.transaction.objectStore('groceries').openCursor(),
-				gymCursor = this.transaction.objectStore('gyms').openCursor(),
-				motorcycleCursor = this.transaction.objectStore('motorcycles').openCursor(),
-				timesheetCursor = this.transaction.objectStore('timesheets').openCursor(),
-				configurationsObject = this.transaction.objectStore('configurations').get(1)
+			var self = this,
+				transaction = App.indexedDB.db.transaction(['timesheets', 'expenses', 'foods', 'groceries', 'gyms', 'motorcycles', 'timesheets', 'configurations'], 'readonly');
+			var	expenseCursor = transaction.objectStore('expenses').openCursor(),
+				foodCursor = transaction.objectStore('foods').openCursor(),
+				groceryCursor = transaction.objectStore('groceries').openCursor(),
+				gymCursor = transaction.objectStore('gyms').openCursor(),
+				motorcycleCursor = transaction.objectStore('motorcycles').openCursor(),
+				timesheetCursor = transaction.objectStore('timesheets').openCursor(),
+				configurationsObject = transaction.objectStore('configurations').get(1)
 			;
 
 			expenseCursor.onsuccess = function(e) {
@@ -88,32 +87,47 @@ define([
 			};
 
 			configurationsObject.onsuccess = function(e) {
-				self.configurations = e.target.result;
-				self.cloud = new Firebase('https://jamesapp.firebaseIO.com');
-				self.auth = new FirebaseSimpleLogin(self.cloud, function(error, user) {
-					if (error) {
-						console.error(error);
-						self.$el.find('#spanMessage').removeClass();
-						self.$el.find('#spanMessage').addClass('col-xs-12 text-center alert alert-danger');
-						self.$el.find('#spanMessage').html('[AUTH ERROR] code: ' + error.code + ' message: ' + error.message).fadeIn().delay(5000).fadeOut();
-					} else if (user) {
-						self.$el.find('#spanMessage').removeClass();
-						self.$el.find('#spanMessage').addClass('col-xs-12 text-center alert alert-success');
-						self.$el.find('#spanMessage').html('User is logged...').fadeIn().delay(5000).fadeOut();
-					} else {
-						console.log('user logout');
-						self.$el.find('#spanMessage').removeClass();
-						self.$el.find('#spanMessage').addClass('col-xs-12 text-center alert alert-info');
-						self.$el.find('#spanMessage').html('user logout!').fadeIn().delay(5000).fadeOut();
-					}
-				});
+				if (e.target.result == undefined) {
+					self.$el.find('#spanMessage').removeClass();
+					self.$el.find('#spanMessage').addClass('col-xs-12 text-center alert alert-danger');
+					self.$el.find('#spanMessage').html('[ERROR] Need save information about configuration before sync data.').fadeIn();
+				} else {
+					self.configurations = e.target.result;
+					self.cloud = new Firebase('https://jamesapp.firebaseIO.com');
+					self.auth = new FirebaseSimpleLogin(self.cloud, function(error, user) {
+						if (error) {
+							console.error(error);
+							self.$el.find('#spanMessage').removeClass();
+							self.$el.find('#spanMessage').addClass('col-xs-12 text-center alert alert-danger');
+							self.$el.find('#spanMessage').html('[AUTH ERROR] code: ' + error.code + ' message: ' + error.message).fadeIn().delay(5000).fadeOut();
+						} else if (user) {
+							self.$el.find('#spanMessage').removeClass();
+							self.$el.find('#spanMessage').addClass('col-xs-12 text-center alert alert-success');
+							self.$el.find('#spanMessage').html('User is logged: ' + user.email).fadeIn().delay(5000).fadeOut();
+							self.$el.find('.disabled').removeClass('disabled');
+						} else {
+							console.log('user logout');
+							self.$el.find('#spanMessage').removeClass();
+							self.$el.find('#spanMessage').addClass('col-xs-12 text-center alert alert-info');
+							self.$el.find('#spanMessage').html('user logout!').fadeIn().delay(5000).fadeOut();
+						}
+					});
 
-				self.auth.login('password', {
-					email: self.configurations.cloudAuth.email
-					, password: self.configurations.cloudAuth.password
-					, rememberMe: true
-				});
+					self.auth.login('password', {
+						email: self.configurations.cloudAuth.email
+						, password: self.configurations.cloudAuth.password
+						, rememberMe: true
+					});
+				}
 			};
+
+// transaction.objectStore('configurations').clear();
+		}
+
+		, onShow: function() {
+			this.$el.find('#spanMessage').removeClass();
+			this.$el.find('#spanMessage').addClass('col-xs-12 text-center alert alert-warning');
+			this.$el.find('#spanMessage').html('Contacting cloud...').fadeIn();
 		}
 
 		, saveOnCloud: function(ev) {
@@ -138,40 +152,42 @@ define([
 			ev.preventDefault();
 
 			// FIXME add to all entities and put code inside onsuccess - error with async request
-			var self = this;
-			this.transaction = App.indexedDB.db.transaction(['timesheets', 'expenses', 'foods', 'groceries', 'gyms', 'motorcycles', 'timesheets', 'configurations'], 'readwrite');
+			var self = this,
+				transaction = App.indexedDB.db.transaction(['timesheets', 'expenses', 'foods', 'groceries', 'gyms', 'motorcycles', 'timesheets', 'configurations'], 'readwrite')
+			;
 
-			this.transaction.objectStore('expenses').clear().onsuccess = function(event) {
-				console.log('Expenses cleared...');
-			};
-			this.transaction.objectStore('foods').clear().onsuccess = function(event) {
-				console.log('Foods cleared...');
-			};
-			this.transaction.objectStore('groceries').clear().onsuccess = function(event) {
-				console.log('Groceries cleared...');
-			};
-			this.transaction.objectStore('gyms').clear().onsuccess = function(event) {
-				console.log('Gyms cleared...');
-			};
-			this.transaction.objectStore('motorcycles').clear().onsuccess = function(event) {
-				console.log('Motorcycles cleared...');
-			};
-			this.transaction.objectStore('timesheets').clear().onsuccess = function(event) {
-				console.log('Timesheets cleared...');
-			};
-			this.transaction.objectStore('configurations').clear().onsuccess = function(event) {
-				console.log('Configurations cleared...');
-			};
+
+			// this.transaction.objectStore('foods').clear().onsuccess = function(event) {
+			// 	console.log('Foods cleared...');
+			// };
+			// this.transaction.objectStore('groceries').clear().onsuccess = function(event) {
+			// 	console.log('Groceries cleared...');
+			// };
+			// this.transaction.objectStore('gyms').clear().onsuccess = function(event) {
+			// 	console.log('Gyms cleared...');
+			// };
+			// this.transaction.objectStore('motorcycles').clear().onsuccess = function(event) {
+			// 	console.log('Motorcycles cleared...');
+			// };
+			// this.transaction.objectStore('timesheets').clear().onsuccess = function(event) {
+			// 	console.log('Timesheets cleared...');
+			// };
+			// this.transaction.objectStore('configurations').clear().onsuccess = function(event) {
+			// 	console.log('Configurations cleared...');
+			// };
 
 			this.cloud.on('value', function(snapshot) {
 				if (snapshot.val() !== null) {
 					// expenses: self.expenses
-					_.forEach(snapshot.val().expenses, function(element, index, list) {
-						self.transaction.objectStore('expenses').add(element).onsuccess = function (event) {
-							console.log('Re-added expenses id #' + element.id);
-						};
-					});
-					console.log(snapshot.val().expenses);
+					transaction.objectStore('expenses').clear().onsuccess = function(event) {
+						console.log('Expenses cleared...');
+						_.forEach(snapshot.val().expenses, function(element, index, list) {
+							transaction.objectStore('expenses').add(element).onsuccess = function (event) {
+								console.log('Re-added expenses id #' + element.id);
+							};
+						});
+					};
+					
 					// , foods: self.foods
 					// , groceries: self.groceries
 					// , gyms: self.gyms
--- app/templates/backup.tpl
+++ app/templates/backup.tpl
@@ -2,18 +2,18 @@
 <h2>Cloud</h2>
 <div class="container-fluid">
     <div class="row">
-        <a id="btnSaveOnCloud" href="#" class="btn btn-primary col-xs-12"><i class="glyphicon glyphicon-cloud-upload"></i> Save on Cloud!</a>
+        <a id="btnSaveOnCloud" href="#" class="btn btn-primary col-xs-12 disabled"><i class="glyphicon glyphicon-cloud-upload"></i> Save on Cloud!</a>
         <br />
         <br />
-        <a id="btnSyncWithCloud" href="#" class="btn btn-primary col-xs-12"><i class="glyphicon glyphicon-cloud-download"></i> Sync with Cloud!</a>
+        <a id="btnSyncWithCloud" href="#" class="btn btn-primary col-xs-12 disabled"><i class="glyphicon glyphicon-cloud-download"></i> Sync with Cloud!</a>
     </div>
     <hr />
     <h2>Disk</h2>
     <div class="row">
-        <a id="btnSaveOnDisk" href="#" class="btn btn-info col-xs-12"><i class="glyphicon glyphicon-save"></i> Save on Disk!</a>
+        <a id="btnSaveOnDisk" href="#" class="btn btn-info col-xs-12 disabled"><i class="glyphicon glyphicon-save"></i> Save on Disk!</a>
         <br />
         <br />
-        <a id="btnSyncWithDisk" href="#" class="btn btn-info col-xs-12"><i class="glyphicon glyphicon-open"></i> Sync with Disk!</a>
+        <a id="btnSyncWithDisk" href="#" class="btn btn-info col-xs-12 disabled"><i class="glyphicon glyphicon-open"></i> Sync with Disk!</a>
     </div>
     <br />
     <div class="row">
