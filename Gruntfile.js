efc6f48e53118882d536f3f13e851a731c0bb103
Jefferson Campos <foguinho.peruca@gmail.com>
2014-07-18 00:27:41 -0300

    Grunt cssmin
    Setup done! But, not working as expected... :(

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,6 @@
 'use strict';
 
 module.exports = function(grunt) {
-	// console.log(grunt.file.readJSON('package.json').version);
-
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
@@ -11,41 +9,41 @@ module.exports = function(grunt) {
 			'* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
 			' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
 
-		jshint: {
-			options: {
-				jshintrc: '.jshintrc'
-			},
-			gruntfile: {
-				src: 'Gruntfile.js'
-			},
-			lib: {
-				options: {
-					jshintrc: '.jshintrc'
-				},
-				src: [
-					'app/js/**/*.js'
-					, '!app/js/libs/**/*.js'
-				]
-			},
-			test: {
-				src: ['app/test/**/*.js']
-			}
-		},
+		// jshint: {
+		// 	options: {
+		// 		jshintrc: '.jshintrc'
+		// 	},
+		// 	gruntfile: {
+		// 		src: 'Gruntfile.js'
+		// 	},
+		// 	lib: {
+		// 		options: {
+		// 			jshintrc: '.jshintrc'
+		// 		},
+		// 		src: [
+		// 			'app/js/**/*.js'
+		// 			, '!app/js/libs/**/*.js'
+		// 		]
+		// 	},
+		// 	test: {
+		// 		src: ['app/test/**/*.js']
+		// 	}
+		// },
 
-		watch: {
-			gruntfile: {
-				files: '<%= jshint.gruntfile.src %>',
-				tasks: ['jshint:gruntfile']
-			},
-			// lib: {
-			// 	files: '<%= jshint.lib.src %>',
-			// 	tasks: ['jshint:lib', 'nodeunit']
-			// },
-			// test: {
-			// 	files: '<%= jshint.test.src %>',
-			// 	tasks: ['jshint:test', 'nodeunit']
-			// },
-		},
+		// watch: {
+		// 	gruntfile: {
+		// 		files: '<%= jshint.gruntfile.src %>',
+		// 		tasks: ['jshint:gruntfile']
+		// 	}
+		// 	, lib: {
+		// 		files: '<%= jshint.lib.src %>',
+		// 		tasks: ['jshint:lib', 'nodeunit']
+		// 	}
+		// 	, test: {
+		// 		files: '<%= jshint.test.src %>',
+		// 		tasks: ['jshint:test', 'nodeunit']
+		// 	}
+		// },
 
 		requirejs: {
 			compile: {
@@ -57,12 +55,26 @@ module.exports = function(grunt) {
 				}
 			}
 		}
+
+		, cssmin: {
+			combine: {
+				files: {
+					// 'dist/main.min.css': ['app/css/main.css', 'app/js/libs/bower/bootstrap/dist/css/bootstrap.min.css', 'app/js/libs/bower/bootstrap/dist/css/bootstrap-theme.min.css', 'app/css/jquery-ui/jquery-ui.min.css'],
+					'dist/main.min.css': ['app/css/main.css'],
+					options: {
+						keepSpecialComments: 0
+					}
+				}
+			}
+		}
 	});
 
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-watch');
+	// grunt.loadNpmTasks('grunt-contrib-jshint');
+	// grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-requirejs');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-	grunt.registerTask('default', ['jshint', 'requirejs']);
+	// grunt.registerTask('default', ['jshint', 'requirejs']);
+	grunt.registerTask('build', ['requirejs', 'cssmin']);
 
 };
