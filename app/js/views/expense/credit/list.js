92705917e99767b4a97213ce0c2b6bb986fbd3e0
Jefferson <foguinho.peruca@gmail.com>
2014-11-17 15:58:37 -0200

    Credit
    Added more implementation

--- app/js/views/expense/credit/list.js
+++ app/js/views/expense/credit/list.js
@@ -5,6 +5,7 @@ define([
 	, 'views/list'
 ], function (_, Marionette, ExpenseModel, ListView) {
 
+	// TODO must implement tagName and template
 	var ItemView = Marionette.ItemView.extend({
 		tagName: function() {
 			return "tr id='row" + this.options.entity.value + this.model.get("id") + "'";
@@ -18,27 +19,48 @@ define([
 		}
 	});
 
+	// FIXME implement:
+	/*
+	 * Must implement the follow:
+	 * - itemView (tagName and template)
+	 * - template of composite view
+	 * - Must define objectStore (string)
+	 */
 	var CompositeView = Marionette.CompositeView.extend({
 		itemView: ItemView
+		, className: 'box'
         , tagName: 'div'
+		, itemViewContainer: '#tbodyItem'
 		, events: {
-			"click .delete-instrutor": "destroy",
-			"click #btnChooseInstrutor": "choose"
+			"click .btn-danger": "delete"
 		}
-        , initialize: function(turma, entity) {
-            this.model = turma;
-			this.itemViewOptions = {entity: entity};
-			if (turma.id != undefined) {
-				switch (entity) {
-				case Utils.Turma.INSTRUTOR:
-					this.collection = new CollectionInstrutor({turma: turma.toJSON()});
-					this.collection.fetch({async: false});
-					this.template = InstrutorCompositeTemplate;
-					this.itemViewContainer = "#tbodyItemInstrutorList";
-					break;
-				}
-			}
+        , initialize: function(coll, model) {
+			this.collection = coll;
+            this.model = model;
+			// this.itemViewOptions = {entity: entity};
+			// if (turma.id != undefined) {
+			// 	switch (entity) {
+			// 	case Utils.Turma.INSTRUTOR:
+			// 		this.collection = new CollectionInstrutor({turma: turma.toJSON()});
+			// 		this.collection.fetch({async: false});
+			// 		this.template = InstrutorCompositeTemplate;
+			// 		this.itemViewContainer = "#tbodyItemInstrutorList";
+			// 		break;
+			// 	}
+			// }
         }
+		// TODO implement it!
+		, delete: function(ev) {
+			ev.preventDefault();
+
+			console.log('Not implemented yet!');
+			// var self = this;
+			// var id = parseInt(ev.currentTarget.getAttribute('id'));
+			// App.indexedDB.db.transaction([this.objectStore], 'readwrite').objectStore(this.objectStore).delete(id).onsuccess = function(e) {
+			// 	var model = self.collection.where({id: id});
+			// 	self.collection.remove(model);
+			// };
+		}
     });
 
 	var LayoutView = Marionette.Layout.extend({
@@ -47,18 +69,20 @@ define([
         template: 'app/templates/expense/credit/list.tpl',
         regions: {
             regionOwner: '#regionOwner',
-            regionCredit: '#regiionCredit'
+            regionCredit: '#regionCredit'
         },
 
-		initialize: function(turma) {
-			this.model = turma;
+		initialize: function(owners, credits) {
+			this.owners = owners;
+			this.credits = credits;
+			// this.model = turma;
 		},
 
         onRender: function() {
 			this.regionTurma.show(new TurmaView(this.model));
 			if (this.model.get('id') != undefined) {
-				this.regionAluno.show(new baseCredit(this.model, Utils.Turma.ALUNO));
-				this.regionInstrutor.show(new CompositeView(this.model, Utils.Turma.INSTRUTOR));
+				this.regionOwner.show(new baseCredit(this.model, Utils.Turma.ALUNO));
+				this.regionCredit.show(new CompositeView(this.model, Utils.Turma.INSTRUTOR));
 			}
 			// console.debug(this.model);
 			this.bindView = new BindingView({
