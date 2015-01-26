14d3460850ad713d8dfeef56122e562367e55550
Jefferson <foguinho.peruca@gmail.com>
2015-01-25 23:35:42 -0200

    Test
    Removed more useless test

--- app/test/jasmine/spec/gymSpec.js
+++
@@ -1,8 +0,0 @@
-// define(['../../../../js/app'], function (App) {
-define(['app'], function (App) {
-	return describe('Gym.', function() {
-		it('Should implement test.', function() {
-			return true;
-		});
-	});
-});
