c540395fec62faefd5212442f95877aa835bad10
Jefferson <foguinho.peruca@gmail.com>
2015-03-26 18:26:59 -0300

    Menu
    Removed extra markup for nav class;

--- app/js/views/menu.js
+++ app/js/views/menu.js
@@ -1,13 +1,11 @@
 define([
 	'marionette'
 	, 'app'
-	, 'text!../../templates/menu.tpl'
-], function (Marionette, App, Template){
+], function (Marionette, App){
 	var ItemView = Marionette.ItemView.extend({
-		// template: Template,
 		template: 'app/templates/menu.tpl',
-		tagName: 'div',
-		className: 'navbar navbar-inverse navbar-static-top'
+		tagName: 'div'
+		// , className: 'navbar navbar-inverse navbar-static-top'
 	});
 
 	return ItemView;
