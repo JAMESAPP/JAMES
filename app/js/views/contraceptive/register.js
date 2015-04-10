ca555a7c8f5ada96066c42f0883d9ffbf92fb7ec
Jefferson Campos <foguinho.peruca@gmail.com>
2015-04-10 11:00:38 -0300

    Contraceptive
    Cleaned register view;

--- app/js/views/contraceptive/register.js
+++ app/js/views/contraceptive/register.js
@@ -1,35 +1,13 @@
 define([
 	'marionette'
-	, 'underscore'
-	, 'app'
-	, 'james-data'
 	, 'views/register'
 	, 'models/setting'
 	, 'jquerymask'
 	, 'jqueryui'
-], function (Marionette, _, App, JAMES_DATA, RegisterView, SettingModel) {
+], function (Marionette, RegisterView, SettingModel) {
 	var ItemView = RegisterView.extend({
 		template: 'app/templates/contraceptive/register.tpl'
 		, objectStore: 'contraceptives'
-		// , serializeData: function() {
-		// 	var attrToView = _.clone(this.attributes),
-		// 		accountFrom,
-		// 		accountTo
-		// 	;
-
-		// 	// attrToView.selectCategory = JAMES_DATA.Financial.getChartOfAccounts();
-		// 	attrToView.accountFrom = JAMES_DATA.Financial.getChartOfAccounts();
-		// 	attrToView.accountTo = JAMES_DATA.Financial.getChartOfAccounts();
-
-		// 	if (attrToView.id != undefined) {
-		// 		attrToView.accountFrom.unshift(attrToView.accountFrom);
-		// 		attrToView.accountTo.unshift(attrToView.accountTo);
-
-		// 		// attrToView.selectCategory.unshift(attrToView.category);
-		// 	}
-
-		// 	return attrToView;
-		// }
 		, onRender: function() {
 			var self = this,
 				setting = new SettingModel(),
